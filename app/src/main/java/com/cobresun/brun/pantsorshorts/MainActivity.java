335686b16fa517c6fa973c33bc6b38aa78612e47
Sunny Nagam <sunnynagam1@gmail.com>
2018-08-13 22:40:42 -0600

    Implemented basic button and image switching

--- app/src/main/java/com/cobresun/brun/pantsorshorts/MainActivity.java
+++ app/src/main/java/com/cobresun/brun/pantsorshorts/MainActivity.java
@@ -2,6 +2,8 @@ package com.cobresun.brun.pantsorshorts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View v){
+        System.out.println("Main Button Pressed");
+        ImageView img=(ImageView)findViewById(R.id.imageView);
+
+        if(img.getTag() == "shorts"){
+            img.setTag("pants");
+            img.setImageResource(R.drawable.pants);
+        }
+        else{
+            img.setTag("shorts");
+            img.setImageResource(R.drawable.shorts);
+        }
+        /*
+        if(img.getVisibility() == View.VISIBLE)
+            img.setVisibility(View.INVISIBLE);
+        else
+            img.setVisibility(View.VISIBLE);
+        */
     }
 }
