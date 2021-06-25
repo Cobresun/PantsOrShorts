360ed7fd6dcba13a841f97166cd27f5002f3b307
Brian Norman <briankn8@gmail.com>
2021-06-24 18:17:43 -0600

    Who thought I'd ever write tests

---
+++ app/src/test/java/com/cobresun/brun/pantsorshorts/TemperatureTest.kt
@@ -0,0 +1,32 @@
+package com.cobresun.brun.pantsorshorts
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class TemperatureTest {
+    @Test
+    fun temperature_CorrectCelsiusToFahrenheit_ReturnsTrue() {
+        val celsius = Temperature(8, TemperatureUnit.CELSIUS)
+        val expectedFahrenheit = Temperature(46, TemperatureUnit.FAHRENHEIT)
+        assertThat(celsius.toFahrenheit()).isEqualTo(expectedFahrenheit)
+    }
+
+    @Test
+    fun temperature_CorrectFahrenheitToCelsius() {
+        val fahrenheit = Temperature(8, TemperatureUnit.FAHRENHEIT)
+        val expectedCelsius = Temperature(-13, TemperatureUnit.CELSIUS)
+        assertThat(fahrenheit.toCelsius()).isEqualTo(expectedCelsius)
+    }
+
+    @Test
+    fun temperature_CorrectFahrenheitToFahrenheit() {
+        val fahrenheit = Temperature(8, TemperatureUnit.FAHRENHEIT)
+        assertThat(fahrenheit.toFahrenheit()).isEqualTo(fahrenheit)
+    }
+
+    @Test
+    fun temperature_CorrectCelsiusToCelsius() {
+        val celsius = Temperature(8, TemperatureUnit.CELSIUS)
+        assertThat(celsius.toCelsius()).isEqualTo(celsius)
+    }
+}
