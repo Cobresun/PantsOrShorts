b690b3c165c118a1936a9a88646e43108504c75b
Brian Norman <briankn8@gmail.com>
2022-07-03 11:33:03 -0400

    Will return subLocality if no locality which finally shows Brooklyn

--- app/src/main/java/com/cobresun/brun/pantsorshorts/Locator.kt
+++ app/src/main/java/com/cobresun/brun/pantsorshorts/Locator.kt
@@ -3,7 +3,6 @@ package com.cobresun.brun.pantsorshorts
 import android.location.Geocoder
 import android.location.Location
 import android.util.Log
-import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -13,7 +12,7 @@ class Locator @Inject constructor(private val geocoder: Geocoder) {
     fun getCityName(location: Location): String? {
         return try {
             val address = geocoder.getFromLocation(location.latitude, location.longitude, 1)[0]
-            address?.locality
+            address?.locality ?: address?.subLocality
         } catch (e: Exception) {
             Log.e(this@Locator.toString(), e.toString())
             null
