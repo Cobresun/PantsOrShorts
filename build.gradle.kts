cf7166a6b776131aaf4a9acc0607e0085ee2e892
brian-norman <briankn8@gmail.com>
2024-07-13 09:04:48 -0400

    Upgrade gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     val hiltVersion = "2.49"
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("com.android.tools.build:gradle:8.5.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
@@ -13,7 +13,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "7.4.0" apply false
+    id("com.android.application") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("org.jetbrains.kotlin.kapt") version "1.8.0" apply false
     id("com.google.dagger.hilt.android") version "2.49" apply false
